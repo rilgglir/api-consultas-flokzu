7133aa96c8a8fc80a187bf316ce5148fe128d28e
rilgglir <rilgglir@gmail.com>
2025-02-11 02:16:53 -0600

    API Postal add

--- src/controllers/postal.controller.js
+++ src/controllers/postal.controller.js
@@ -1,1 +1,23 @@
-export const getPostal = (req, res) => res.send('obteniendo info de Codigo Postal');
+import { pool } from '../db.js';
+
+export const getPostal = async(req, res) => {
+  
+    try{
+
+        const [rows] = await pool.query('SELECT * FROM postal WHERE Codigo_Postal = ?', [req.params.cp]);
+        
+        if (rows.length <= 0) return res.status(404).json({
+            message: 'No se encontró el codigo postal'
+        });
+        
+        console.log(rows);
+        
+        res.json(rows[0]);  
+
+    }catch (error){
+        
+        return res.status(500).json({
+            message : "Algo malo ocurrio"
+        })
+    }
+};
