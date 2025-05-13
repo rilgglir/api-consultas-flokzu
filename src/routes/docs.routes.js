126658b30985e4cff5e8e0024afb37022590083d
Ricardo Ivan López Gutiérrez <87155251+rilgglir@users.noreply.github.com>
2025-05-13 03:26:29 -0600

    Update docs.routes.js
    se agrega ruta para recibir programa

--- src/routes/docs.routes.js
+++ src/routes/docs.routes.js
@@ -3,7 +3,7 @@ import { getDocs } from "../controllers/docs.controller.js";
 
 const router = Router();
 
-router.get('/docs/:matricula', getDocs);
+router.get('/docs', getDocs);
 
 
 export default router;
