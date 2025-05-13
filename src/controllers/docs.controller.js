2a17acab3ee8bd338fe52538a0450a027525d439
Ricardo Ivan López Gutiérrez <87155251+rilgglir@users.noreply.github.com>
2025-05-13 03:25:10 -0600

    Update docs.controller.js
    se agrega programa para la busqueda

--- src/controllers/docs.controller.js
+++ src/controllers/docs.controller.js
@@ -3,13 +3,13 @@ import { pool } from '../db.js';
 export const getDocs = async(req, res) => {
   
     try{
-        const matricula = req.params.matricula;
-        const result = await pool.query('SELECT estatus, estatus_titulacion, estatus_expediente_fisico, estatus_expediente_digital FROM base_correspondencia WHERE MATRICULA = $1', [matricula]);
+        const { matricula, programa } = req.query;
+        const result = await pool.query('SELECT estatus, estatus_titulacion, estatus_expediente_fisico, estatus_expediente_digital FROM base_correspondencia WHERE MATRICULA = $1 AND PROGRAMA = $2', [matricula, programa]);
         
         if (result.rows.length <= 0) {
 
             return res.status(404).json({
-                message: 'No se encontró la matrícula'
+                message: 'No se encontró la matrícula y programa indicados'
             });
             
         }
